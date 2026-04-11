c7db53bb62107ff08a30685b031a6def4cee31f7
PRANAY <rautpranay6543@gmail.com>
2026-04-11 19:18:42 +0530

    11april

--- players.js
+++ players.js
@@ -3,12 +3,14 @@ export default function Players() {
 
   content.innerHTML = `
     <div id="src">
-      <input type="text" id="search" placeholder="Search Players">
+      <input type="text" id="search" placeholder="Enter player's name">
       <span><button id="search-btn">Search</button></span>
     </div>
+    <div id="playerBox"></>
   `;
-
   document.getElementById("search-btn").addEventListener("click", async function () {
+      const box=document.getElementById("playerBox");
+      box.innerHTML="";
       const val = document.getElementById("search").value;
 
       if (!val) {
@@ -23,12 +25,30 @@ export default function Players() {
           `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${encodeURIComponent(Name)}`
         );
 
-        const data = await response.json();
+        let data = await response.json();
+        if(data.player[0].strSport!="Soccer"){
+          return alert("Player not found.")
+        }
+        box.innerHTML=playerCard(data);
         console.log(data);
 
+        function playerCard(ev){
+          return `
+            <div id="player-img-box">
+            <img id="playerImg" src=${ev.player[0].strCutout}>
+            </div>
+            <div id="info">
+            <div id="playerName">Name: ${ev.player[0].strPlayer}</div>
+            <div id="playerPos">Position: ${ev.player[0].strPosition}</div>
+            <div id="playerGen">Gender: ${ev.player[0].strGender}</div>
+            <div id="playerBorn">Birth: ${ev.player[0].dateBorn}</div>
+            <div id="playerTeam">Team: ${ev.player[0].strTeam}</div>
+            </div>
+        `
+        }
 
       } catch (error) {
-        console.error("Error:", error);
+        console.error(" Invalid Player Name " );
       }
     });
 }
